d7d87a4c38409c9759427582710d53db0f28637b
Mark <SwiftTactics@gmail.com>
2021-02-13 18:19:19 -0500

    added strong parameters / new form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,26 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  # instantiates a new article, does not save
+  def new
+    @article = Article.new
+  end
+
+  # instantiates a new article, if saved navigates to that article!
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  # Requires article, and allows addition of title / body -- variable for article fields
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
