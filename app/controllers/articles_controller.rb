143757786ef723e64dd2ba52db24c334668bb4f3
Mark <SwiftTactics@gmail.com>
2021-02-13 17:50:55 -0500

    article list clean up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+  # sets what index populates, sets articles variable to for each
   def index
     @articles = Article.all
   end
+  # sets what show populates, finds that article by request parameter
   def show
     @article = Article.find(params[:id])
   end
