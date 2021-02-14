8f29f546b2e50f694be923dbb37aad35e60505c1
Mark <SwiftTactics@gmail.com>
2021-02-13 20:14:48 -0500

    added comments Model

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  
+  # allows Article show to create a new comment.
+  # @article.comments.create adds to comments array in article.
+  # redirect as a form of rendering
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  # sets the required fields for comment addition / editing
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  # sets one to many, can access with @article.comments
+  has_many :comments
+
   # validates title exists, and body is atleast 10 chars
   # Active Record automatically defines model attributes for every table column
   validates :title, presence: true
